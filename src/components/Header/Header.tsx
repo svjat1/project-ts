ea919d875795cebbf8204de8a218d9eccfb11b2e
svjat1 <calipsossss@gmail.com>
2024-03-23 18:53:07 +0200

    style6

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,7 @@ import {useForm} from "react-hook-form";
 
 
 const Header = () => {
-   const {register, reset, handleSubmit, watch} = useForm()
+   const {register, reset, handleSubmit, setValue} = useForm()
     const [searchValue, setSearchValue] = useState<string>(''); //це для форми
 
     const dispatch = useAppDispatch()
@@ -25,7 +25,6 @@ const Header = () => {
     const navigate = useNavigate();
     const clear =()=> {
         dispatch(movieActions.setQuery(''))
-        // dispatch(movieActions.clearQuery);
         reset()
     };
 
@@ -36,20 +35,16 @@ const Header = () => {
                 <div className={css.Search}>
                     <form onSubmit={handleSubmit(clear)}>
                         <input type="text" placeholder="Serch"
-                               // value={searchValue} //закоментовано бо інакше не працюж регістер
-                        onChange={(e)=>{
-                            setSearchValue(e.target.value)
-                            dispatch(movieActions.setQuery(e.target.value))
-                        }}
-                               name='search' {...register('search')}
+                              {...register('search')}
+                               onChange={(e) => {
+                                   setValue('search', e.target.value)
+                                   dispatch(movieActions.setQuery(e.target.value))
+                               }}
                         />
                         <button type={"submit"} >Show All</button>
                         {/*<TextField id="outlined-basic" label="Serch" variant="outlined" />*/}
                     </form>
-                    <form onSubmit={handleSubmit(clear)}>
-                        <input type='text' name='searc' {...register('searc')}/>
-                        <button type='submit'>Save</button>
-                    </form>
+
                 </div>
                 <button onClick={()=> dispatch(movieActions.setGenre())} className={css.GenreButton}>Жанри</button>
                 <div className={css.Theme}>
